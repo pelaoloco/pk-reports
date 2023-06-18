207c880d99c0803f67063b308de593a1cda6e808
pelaoloko <135795054+pelaoloco@users.noreply.github.com>
2023-06-17 20:53:00 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,27 +1,30 @@
+-- I think we need ESX :)
 ESX = exports["es_extended"]:getSharedObject()
 
+-- Command for report (view the /command in config.lua)
 RegisterCommand(Config.ReportCommand, function(source, args, rawCommand)
-    local nam = GetPlayerName(source)
+    local name = GetPlayerName(source)
     local input = lib.inputDialog('Reportar una situacion', {'Motivo'})
 
     if input then 
-        local motivo = input[1]
+        local reason = input[1]
 
-        if motivo == nil or motivo == "" then 
-            TriggerEvent('pk:notif', 'Tu reporte no es válido!', 'error')
+        if reason == nil or reason == "" then 
+            notify('Tu reporte no es válido!', 'error')
         else 
-            TriggerEvent('pk:notif', 'Has enviado un reporte: '..motivo, 'success')
-            TriggerServerEvent('pk:reportlog', motivo)
+            notify('Has enviado un reporte: '..reason, 'success')
+            TriggerServerEvent('pk:reportlog', reason)
         end 
     end 
 end, false)
 
-RegisterNetEvent('pk:notif')
-AddEventHandler('pk:notif', function(msg, tipo)
+
+-- Notify function, this maybe be used for notify something
+function notify(msg, ntype)
     lib.notify({
         title = 'PK Report System',
         description = msg,
         position = 'center-right',
-        type = tipo
+        type = ntype
     })
-end)
+end
